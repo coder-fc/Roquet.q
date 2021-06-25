f8aaffe38b5c7656a96b3002181fbe877e35f941
DevFC <felipech3344@gmail.com>
2021-06-25 00:17:11 -0300

    Form route
    Criação do caminho para envio dos dados do formulário da modal.

--- src/route.js
+++ src/route.js
@@ -6,4 +6,7 @@ route.get('/', (req, res) => res.render("index"))
 route.get('/room', (req, res) => res.render("room"))
 route.get('/create-pass', (req, res) => res.render("create-pass"))
 
+// Formato que o formulario de dentro da modal tem que passar a informação. 
+// route.get('/room/:room/:question/:action')
+
 module.exports = route
