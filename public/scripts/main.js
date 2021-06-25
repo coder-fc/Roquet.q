63a14c5ad9db299867ded0fadd411dd8cb771dd3
DevFC <felipech3344@gmail.com>
2021-06-24 23:40:29 -0300

    Modal ROOM
    Realizamos a codificação do JavaScript para que a modal se comporte de acordo com seu determinado estado.

---
+++ public/scripts/main.js
@@ -0,0 +1,35 @@
+import Modal from './modal.js' 
+
+const modal = Modal()
+
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button') 
+
+//Pegar todos os botões escritos com a classe check 
+const checkButtons = document.querySelectorAll(".actions a.check")
+
+checkButtons.forEach(button => {
+  // Adicionar a escuta
+  button.addEventListener("click", handleClick)
+}) // Pegar quando o marcar como lido for clicado 
+
+// Quando o botão delete for clicado ele abre a modal
+const deleteButton = document.querySelectorAll(".actions a.delete")
+
+deleteButton.forEach(button =>{
+  button.addEventListener("click", (event) =>  handleClick(event, false))
+})
+
+function handleClick(event, check = true) {
+  event.preventDefault()
+  const text = check ? "Marcar como lida " : "Excluir "
+
+  modalTitle.innerHTML = `${text} esta pergunta?`
+  modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+  modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+  check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+   //Abrir modal
+  modal.open()
+} 
