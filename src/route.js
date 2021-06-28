1244469e633435c27e8a73d109e2df71e226f4e1
DevFC <felipech3344@gmail.com>
2021-06-28 00:17:49 -0300

    Implementação BD 1
    Incluindo as primeiras tabelas do banco de dados no código.

--- src/route.js
+++ src/route.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
-const route = express.Router()
 const RoomController = require('./controllers/RoomController')
+
+const route = express.Router()
 
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
@@ -9,7 +10,7 @@ route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'
 route.get('/room/:room', (req, res) => res.render("room"))
 
 // Formato que o formulario de dentro da modal tem que passar a informação. 
-route.post('/room/:room/:question/:action', QuestionController.index)
-route.post('/room/create-room', RoomController.create) 
+route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/create-room', RoomController.create) 
 
 module.exports = route
